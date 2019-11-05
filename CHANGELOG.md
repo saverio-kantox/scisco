0cbda59d346ecf720b1e68a794a025631bfd04e2
Saverio Trioni <saverio.trioni@kantox.com>
2019-11-05 10:37:40 +0100

    Added Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## latest
+
+...
+
+## 0.1.0
+
+- Initial implementation
--- mix.exs
+++ mix.exs
@@ -33,10 +33,7 @@ defmodule Scisco.MixProject do
 
   defp package do
     [
-      # This option is only needed when you don't want to use the OTP application name
-      name: "postgrex",
-      # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* CHANGELOG* src),
+      files: ~w(lib .formatter.exs mix.exs README* CHANGELOG*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/saverio-kantox/#{@app}"}
     ]
